4e931b90980e1ad9218a6764d31fdc950cfb2df6
Somya-Bansal <somyabansal1996@gmail.com>
2022-09-18 16:57:43 -0700

    Add 2 projects

--- src/components/Projects/projects.js
+++ src/components/Projects/projects.js
@@ -15,9 +15,10 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
-
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { IconButton } from '@mui/material';
+import dv_project from "../../images/dv_project.png"
+import hci_project from "../../images/hci_project.png"
+import blog_project from "../../images/blog_project.png"
 
 function Copyright() {
   return (
@@ -37,13 +38,22 @@ const projects = [
     title: "VAST MC2 Challenge",
     description: "Solution to the 2015 VAST MC2 by exploring the in-app communication data collected during the Scott Jones celebration at the DinoFun World. Used five types of visualization techniques to answer the questions asked in the challenge. The system also provides more focused and detailed views of these visualizations by filtering on time and locations in the park.",
     githubRepoLink: "https://github.com/Somya-Bansal/vast-mc2-challenge-2015",
-    demoLink: "https://somya-bansal.github.io/vast-mc2-challenge-2015/"
+    demoLink: "https://somya-bansal.github.io/vast-mc2-challenge-2015/",
+    image: dv_project
   },
   {
-    title: "VAST MC2 Challenge",
-    description: "",
-    githubRepoLink: "https://github.com/Somya-Bansal/vast-mc2-challenge-2015",
-    demoLink: "https://somya-bansal.github.io/vast-mc2-challenge-2015/"
+    title: "Redesigning Zara.com for better UX",
+    description: "Investigating the e-commerce website zara.com with the goal of improving the user experience. The redesign makes it simpler for users to navigate through the website, reduces the number of page changes made by the user in the checkout flow. It also cuts down the total time spent on the task and makes the overall shopping experience on zara.com much simpler.",
+    githubRepoLink: "https://omiuvv.axshare.com/home.html",
+    demoLink: "https://omiuvv.axshare.com/home.html",
+    image: hci_project
+  },
+  {
+    title: "Designed a blogger with React",
+    description: "This was my first React project where I designed and implemented a blog publishing website. It is a complete system with Firebase integration for database and is hosted on Heroku.",
+    githubRepoLink: "https://github.com/Somya-Bansal/blog-with-react-and-firebase",
+    demoLink: "https://blog-with-react-firebase.herokuapp.com/",
+    image: blog_project
   },
 ]
 const theme = createTheme();
@@ -53,8 +63,8 @@ export default function Projects() {
     <ThemeProvider theme={theme}>
       <CssBaseline />
       <AppBar position="sticky">
-        <Toolbar sx={{display:'flex', justifyContent:'space-between'}}>
-          
+        <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+
           <Typography variant="h6" color="inherit" noWrap>
             Somya Bansal
           </Typography>
@@ -62,29 +72,29 @@ export default function Projects() {
             <Button
               variant="button"
               color="text.primary"
-              // href="#"
-              // sx={{ my: 1, mx: 1.5 }}
+            // href="#"
+            // sx={{ my: 1, mx: 1.5 }}
             >
               About Me
             </Button>
             <Button
               variant="button"
               color="text.primary"
-              // href="#"
-              // sx={{ my: 1, mx: 1.5 }}
+            // href="#"
+            // sx={{ my: 1, mx: 1.5 }}
             >
               Projects
             </Button>
             <Button
               variant="button"
               color="text.primary"
-              // href="#"
-              // sx={{ my: 1, mx: 1.5 }}
+            // href="#"
+            // sx={{ my: 1, mx: 1.5 }}
             >
               Contact
             </Button>
           </nav>
-          
+
         </Toolbar>
       </AppBar>
       <main>
@@ -125,12 +135,13 @@ export default function Projects() {
           {/* End hero unit */}
           <Grid container spacing={8}>
             {projects.map((project, index) => (
-              <Grid item key={index} xs={12} sm={6} md={4}>
-                <Card>
+              <Grid item key={index} xs={12} sm={6} md={4} sx={{ display: 'flex' }}>
+                <Card sx={{ display: 'flex', flexDirection:'column' }}>
                   <CardMedia
                     component="img"
-                    image="https://source.unsplash.com/random"
-                    alt="random"
+                    image={project.image}
+                    height="240"
+                    alt="project_peek_image"
                   />
                   <CardContent sx={{ flexGrow: 1 }}>
                     <Typography gutterBottom variant="h5" component="h2">
@@ -141,13 +152,13 @@ export default function Projects() {
                     </Typography>
                   </CardContent>
                   <CardActions>
-                    <IconButton aria-label="add to favorites" href={project.githubRepoLink} target="_blank">
-                      <GitHubIcon />
-                    </IconButton>
+                    <Button href={project.githubRepoLink} target="_blank" variant="outlined" startIcon={<GitHubIcon />}>
+                      Github
+                    </Button>
 
-                    <IconButton aria-label="add to favorites" href={project.demoLink} target="_blank">
-                      <PlayCircleIcon />
-                    </IconButton>
+                    <Button href={project.demoLink} target="_blank" variant="outlined" startIcon={<PlayCircleIcon />}>
+                      Live Demo
+                    </Button>
 
                   </CardActions>
                 </Card>
